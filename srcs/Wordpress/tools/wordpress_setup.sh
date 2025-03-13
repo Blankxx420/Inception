7a1ecd89de8b5988d3853eb9a12726085e0a16a3
Blankxx420 <brguicho.dev@gmail.com>
2025-03-13 16:03:24 +0100

    continuing wordpress implementation

--- srcs/Wordpress/tools/wordpress_setup.sh
+++ srcs/Wordpress/tools/wordpress_setup.sh
@@ -1,12 +1,20 @@
 alias wp="wp --allow-root"
 
 wp core install \
- --url $DOMAIN_NAME \
- -title $WP_TITLE \
- --admin_user $ADMIN_USERNAME \
- --admin_password $ADMIN_PASSWORD \
- --admin_email $ADMIN_EMAIL \
- --locale fr_FR \
- --skip-email
+    --url $DOMAIN_NAME \
+    --title $WP_TITLE \
+    --admin_user $ADMIN_USERNAME \
+    --admin_password $ADMIN_PASSWORD \
+    --admin_email $ADMIN_EMAIL \
+    --locale fr_FR \
+    --skip-email
 
-if (! -f /wordpress/)
+if (! -f /wordpress/wp-config.php)
+then
+    wp config create \
+        --dbname $DB_NAME \
+        --dbuser $DB_USER \
+        --dbpass $DB_PASS \
+        --dbhost mariadb:3306 \
+
+fi
