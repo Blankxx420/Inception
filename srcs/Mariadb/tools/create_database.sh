3dc3c75ecefe1633f6e233381681eac35f747bc9
Blankxx420 <briceguichou.dev@gmail.com>
2025-03-16 23:24:22 +0100

    wordpress error db conect

--- srcs/Mariadb/tools/create_database.sh
+++ srcs/Mariadb/tools/create_database.sh
@@ -3,14 +3,14 @@
 service mariadb start
 
 # Drop user if exists and create new user
-mysql -u root -e "DROP USER IF EXISTS '$DB_USER'@'%';"
-mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -u root -e "DROP USER IF EXISTS '$DB_USER'@'localhost';"
+mysql -u root -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 
 # Create database if it doesn't exist
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
   
 # Grant privileges to the user
-mysql -u root -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'%';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'localhost';"
 
 # Reload privileges to apply changes
 mysql -u root -e "FLUSH PRIVILEGES;"
--- srcs/Wordpress/tools/wordpress_setup.sh
+++ srcs/Wordpress/tools/wordpress_setup.sh
@@ -1,20 +1,30 @@
-alias wp="wp --allow-root"
+#!/bin/bash
 
-wp core install \
-    --url $DOMAIN_NAME \
-    --title $WP_TITLE \
-    --admin_user $ADMIN_USERNAME \
-    --admin_password $ADMIN_PASSWORD \
-    --admin_email $ADMIN_EMAIL \
+sleep 10
+
+# Ensure WordPress is downloaded first
+wp core download --path="/var/www/html/" --allow-root
+
+
+# Check if wp-config.php does not exist
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create \
+        --allow-root \
+		--force \
+        --dbname=$DB_NAME \
+        --dbuser=$DB_USER \
+        --dbpass=$DB_PASS \
+        --dbhost=$DB_HOST \
+        --path="/var/www/html/"
+
+	wp core install \
+	--allow-root \
+	--path="/var/www/html/" \
+    --url=$DOMAIN_NAME \
+    --title=$WP_TITLE \
+    --admin_user=$ADMIN_USERNAME \
+    --admin_password=$ADMIN_PASSWORD \
+    --admin_email=$ADMIN_EMAIL \
     --locale fr_FR \
     --skip-email
-
-if (! -f /wordpress/wp-config.php)
-then
-    wp config create \
-        --dbname $DB_NAME \
-        --dbuser $DB_USER \
-        --dbpass $DB_PASS \
-        --dbhost mariadb:3306 \
-
 fi
